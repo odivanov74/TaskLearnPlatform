1a337fe067e0526adbdc115e157319005698cbaf
odivanov74 <odivanov74@gmail.com>
2023-01-21 08:52:36 +0700

    initial commit HW-46

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Sql_HW46
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SelectAllFromTable_Button = new System.Windows.Forms.Button();
+            this.Tables_ComboBox = new System.Windows.Forms.ComboBox();
+            this.Update_Button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(409, 325);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // SelectAllFromTable_Button
+            // 
+            this.SelectAllFromTable_Button.Location = new System.Drawing.Point(670, 31);
+            this.SelectAllFromTable_Button.Name = "SelectAllFromTable_Button";
+            this.SelectAllFromTable_Button.Size = new System.Drawing.Size(118, 23);
+            this.SelectAllFromTable_Button.TabIndex = 1;
+            this.SelectAllFromTable_Button.Text = "Вывести";
+            this.SelectAllFromTable_Button.UseVisualStyleBackColor = true;
+            this.SelectAllFromTable_Button.Click += new System.EventHandler(this.SelectAllFromTable_Button_Click);
+            // 
+            // Tables_ComboBox
+            // 
+            this.Tables_ComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Tables_ComboBox.FormattingEnabled = true;
+            this.Tables_ComboBox.Location = new System.Drawing.Point(440, 31);
+            this.Tables_ComboBox.Name = "Tables_ComboBox";
+            this.Tables_ComboBox.Size = new System.Drawing.Size(214, 21);
+            this.Tables_ComboBox.TabIndex = 2;
+            // 
+            // Update_Button
+            // 
+            this.Update_Button.Location = new System.Drawing.Point(12, 371);
+            this.Update_Button.Name = "Update_Button";
+            this.Update_Button.Size = new System.Drawing.Size(137, 42);
+            this.Update_Button.TabIndex = 3;
+            this.Update_Button.Text = "Сохранить изменения";
+            this.Update_Button.UseVisualStyleBackColor = true;
+            this.Update_Button.Click += new System.EventHandler(this.Update_Button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Update_Button);
+            this.Controls.Add(this.Tables_ComboBox);
+            this.Controls.Add(this.SelectAllFromTable_Button);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button SelectAllFromTable_Button;
+        private System.Windows.Forms.ComboBox Tables_ComboBox;
+        private System.Windows.Forms.Button Update_Button;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sql_HW46
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        DataSet data = new DataSet();
+        SqlDataAdapter adapter;
+        DataTable table;
+        SqlCommandBuilder commandBuilder;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(@"Data Source=DESKTOP-MHB46B8\SQLEXPRESS;Initial catalog=TaskLearnPlatform;Integrated Security=true;");
+            SqlCommand command = new SqlCommand("select * from TaskLearnPlatform.information_schema.tables", connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            Tables_ComboBox.Items.Add("All");
+            while (reader.Read())
+            {
+                Tables_ComboBox.Items.Add(reader.GetString(2));
+            }
+            Tables_ComboBox.SelectedIndex = 0;
+            connection.Close();
+        }
+
+        private void SelectAllFromTable_Button_Click(object sender, EventArgs e)
+        {
+            string cmd = "";
+            //чистка dataGridView
+            if (table != null)
+            {
+                while (table.Rows.Count > 0)
+                {
+                    table.Rows.RemoveAt(0);
+                }
+                while (table.Columns.Count > 0)
+                {
+                    table.Columns.RemoveAt(0);
+                }
+                dataGridView1.DataSource = table;
+            }
+
+            if (Tables_ComboBox.SelectedItem.ToString() == "All")
+            {
+                foreach (var item in Tables_ComboBox.Items)
+                {                    
+                    if (item.ToString() != "All")
+                    {
+                        cmd = $"select * from {item.ToString()};";
+                        commandBuilder = new SqlCommandBuilder(adapter);
+                        adapter = new SqlDataAdapter(cmd, connection);
+                        adapter.Fill(data);                      
+                    }
+                }
+            }
+            else
+            {
+                cmd = $"Select * from {Tables_ComboBox.SelectedItem.ToString()};";
+                adapter = new SqlDataAdapter(cmd, connection);
+                commandBuilder = new SqlCommandBuilder(adapter);
+                adapter.Fill(data);
+            }
+            
+            table = data.Tables[0];
+            dataGridView1.DataSource = table;
+        }
+
+        private void Update_Button_Click(object sender, EventArgs e)
+        {
+            
+            adapter.Update(data);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string res = "";
+            res += dataGridView1.SelectedCells[0].Value.ToString() + Environment.NewLine;
+            res += dataGridView1.SelectedCells[0].ColumnIndex.ToString() + ":" + dataGridView1.SelectedCells[0].RowIndex.ToString() + Environment.NewLine;
+            res += data.Tables[0].Columns[dataGridView1.SelectedCells[0].ColumnIndex].ColumnName + Environment.NewLine;
+            res += dataGridView1.SelectedCells[0].Value;
+            MessageBox.Show(res);
+        }
+    }
+}
